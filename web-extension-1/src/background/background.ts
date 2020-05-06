6439c31768d3fa2980ec8c3c23f889370809241e
Matthieu FOREL <matthieu.forel@epitech.eu>
2020-05-06 14:52:09 +0200

    implemented very simple tab-specific counter

--- web-extension-1/src/background/background.ts
+++ web-extension-1/src/background/background.ts
@@ -1,5 +1,41 @@
+interface Itab extends browser.tabs.Tab {
+  totalOpened: number;
+}
+
+let tabs:Itab[] = new Array<Itab>();
+
 const setupBackground = async () => {
   console.log('setup background');
 };
 
+const findOrAddTab = (tab: Itab) : Itab  => {
+  let fTab = tabs.find(t => t.id === tab.id);
+  if (fTab) {
+    return fTab;
+  } else {
+    tab.totalOpened = 0;
+    tabs.push(tab);
+  }
+  return tab;
+}
+
+const popupOpened = (data: any) => {
+  // tied to the active Tab
+  browser.tabs.query({active:true,currentWindow:true}).then(function(tabs){
+      // 'tabs' will be an array with only one element: an Object describing the active tab
+      // in the current window.
+      var currentTab = findOrAddTab(tabs[0] as Itab);
+      currentTab.totalOpened += 1;
+  });
+}
+
 browser.runtime.onInstalled.addListener(setupBackground);
+browser.runtime.onMessage.addListener(popupOpened)
+
+
+// browser.notifications.create({
+//   "type": "basic",
+//   "title": "hello",
+//   "message":"Opened"
+// });
+
--- web-extension-1/src/content_script/content_script.ts
+++ web-extension-1/src/content_script/content_script.ts
@@ -4,3 +4,13 @@ const port = browser.runtime.connect(browser.runtime.id, { name: 'content_script
 port.onDisconnect.addListener(() => {
   console.log('cleanup');
 });
+
+(() => {
+  browser.runtime.sendMessage({
+    action: 'open'
+  })
+});
+
+const messageFormat = (counter: number) => {
+  return `Popup opened ${counter} times on this tab`;
+}
