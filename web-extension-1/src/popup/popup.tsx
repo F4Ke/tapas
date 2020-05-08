e250b9201c7c230554aed847e40f20d0b7482e0f
Matthieu FOREL <matthieu.forel@epitech.eu>
2020-05-08 19:03:40 +0200

    console log

--- web-extension-1/src/popup/popup.tsx
+++ web-extension-1/src/popup/popup.tsx
@@ -2,12 +2,9 @@
 import { currentTabPromise, IPopupEvent } from '../interfaces/tab'
 
 (() => {
-
   // fetch the current and active tab
   // from which the popup as called
-
   currentTabPromise().then(function(tabs) {
-    console.log(tabs)
     if (tabs[0] !== undefined) {
       const tabId = tabs[0].id as number;
       browser.runtime.sendMessage({ active : true, tabId: tabId } as IPopupEvent);
